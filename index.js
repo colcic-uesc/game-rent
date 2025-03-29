11ecdcf6a9508f707814c415d6714c275896fb0b
Christian Oliveira <christian97_6@live.com>
2025-03-29 01:33:00 -0300

    feat: configure an initial web service

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const server = express();
+
+const PORT = 3000;
+
+server.get('/', (req, res) => {
+   res.json({
+      message: 'Hello World',
+   });
+});
+
+server.listen(PORT, (error) => {
+   if (error) {
+      return console.error(error);
+   }
+   console.log(`Server is running on http://localhost:${PORT}`);
+});
