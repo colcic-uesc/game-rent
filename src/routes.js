e7ce44212272ffff3cde3b9aa30b23cd1f7bfac8
Christian Oliveira <christian97_6@live.com>
2025-04-09 20:56:58 -0300

    feat: add users routes

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,17 @@ import { Router } from "express";
 
 const routes = Router();
 
-import HomeController from "./app/controllers/HomeController.js";
+import HomeController from "./app/controllers/HomeController";
+import UsersController from "./app/controllers/UsersController";
 
+// Home
 routes.get("/", HomeController.index);
+
+// Users
+routes.get("/users", UsersController.index);
+routes.get("/users/:id", UsersController.show);
+routes.post("/users", UsersController.create);
+routes.put("/users/:id", UsersController.update);
+routes.delete("/users/:id", UsersController.destroy);
 
 export default routes;
