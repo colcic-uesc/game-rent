869c88de39e95b62b1749b02390197bf5eed9490
Christian Oliveira <christian97_6@live.com>
2025-04-09 20:55:54 -0300

    feat: load models into the app and configure error handling

--- src/app.js
+++ src/app.js
@@ -1,19 +1,40 @@
+import "dotenv/config";
+
 import express from "express";
-import routes from "./routes.js";
+import Youch from "youch";
+import "express-async-errors";
+
+import routes from "./routes";
+
+import "./database/index";
 
 class App {
    constructor() {
       this.server = express();
+
       this.middlewares();
       this.routes();
+      this.exceptionHandler();
    }
 
    middlewares() {
       this.server.use(express.json());
+      this.server.use(express.urlencoded({ extended: false }));
    }
 
    routes() {
       this.server.use(routes);
+   }
+
+   exceptionHandler() {
+      this.server.use(async (err, req, res, next) => {
+         if (process.env.NODE_ENV === "development") {
+            const errors = await new Youch(err, req).toJSON();
+            return res.status(500).json(errors);
+         }
+
+         return res.status(500).json({ error: "Internal server error" });
+      });
    }
 }
 
