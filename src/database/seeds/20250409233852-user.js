d062cfb1e65e2f6d580dbd6e8cf4189f5e8a114a
Christian Oliveira <christian97_6@live.com>
2025-04-09 20:55:02 -0300

    feat: add users seeder

---
+++ src/database/seeds/20250409233852-user.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = {
+   up: (queryInterface, Sequelize) => {
+      return queryInterface.bulkInsert(
+         "users",
+         [
+            {
+               name: "John Doe",
+               email: "john@gmail.com",
+               password_hash: "$2a$08$e0b1c4f8d3a5e7c5f8d3eO9Q6Z5J6F5F5F5F5F5F5F5F5F5F5F5",
+               created_at: new Date(),
+               updated_at: new Date(),
+            },
+            {
+               name: "Jane Doe",
+               email: "jane@gmail.com",
+               password_hash: "$2a$08$e0b1c4f8d3a5e7c5f8d3eO9Q6Z5J6F5F5F5F5F5F5F5F5F5F5F5",
+               created_at: new Date(),
+               updated_at: new Date(),
+            }
+         ],
+         {}
+      );
+   },
+
+   down: (queryInterface, Sequelize) => {
+      return queryInterface.bulkDelete("users", null, {});
+   },
+};
