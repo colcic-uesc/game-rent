2f1df824a9708884e96377644aceee7410832221
Christian Oliveira <christian97_6@live.com>
2025-04-25 19:09:33 -0300

    feat: add swagger configuration

---
+++ src/config/swagger.js
@@ -0,0 +1,26 @@
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+   definition: {
+      openapi: "3.0.0",
+      info: {
+         title: "API Documentation",
+         version: "1.0.0",
+         description: "Documentation for Game Rent API",
+      },
+      servers: [
+         {
+            url: `http://localhost:${process.env.PORT}`,
+         },
+      ],
+   },
+   apis: ["./src/routes.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+export {
+   specs,
+   swaggerUi
+}
