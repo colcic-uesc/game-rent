e435e0b9895c064a5e6dac7cb599d8eea94aeeec
Christian Oliveira <christian97_6@live.com>
2025-04-25 03:10:18 -0300

    feat: add game migration

---
+++ src/database/migrations/20250425034054-create-game.js
@@ -0,0 +1,70 @@
+"use strict";
+
+module.exports = {
+   up: (queryInterface, Sequelize) => {
+      return queryInterface.createTable("games", {
+         id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER,
+         },
+         title: {
+            type: Sequelize.STRING,
+            allowNull: false,
+         },
+         description: {
+            type: Sequelize.STRING,
+            allowNull: false,
+         },
+         price: {
+            type: Sequelize.FLOAT,
+            allowNull: false,
+         },
+         status: {
+            type: Sequelize.ENUM("available", "paused", "unavailable"),
+            defaultValue: "available",
+            allowNull: false,
+         },
+         yt_link: {
+            type: Sequelize.STRING,
+            allowNull: false,
+         },
+         capa_jogo: {
+            type: Sequelize.STRING,
+            allowNull: false,
+         },
+         platform_id: {
+            type: Sequelize.INTEGER,
+            references: {
+               model: "platforms",
+               key: "id",
+            },
+            onUpdate: "CASCADE",
+            onDelete: "SET NULL",
+            allowNull: true,
+         },
+         genre_id: {
+            type: Sequelize.INTEGER,
+            references: {
+               model: "genres",
+               key: "id",
+            },
+            onUpdate: "CASCADE",
+            onDelete: "SET NULL",
+            allowNull: true,
+         },
+         created_at: {
+            allowNull: false,
+            type: Sequelize.DATE,
+         },
+         updated_at: {
+            allowNull: false,
+            type: Sequelize.DATE,
+         },
+      });
+   },
+   down: (queryInterface, Sequelize) => {
+      return queryInterface.dropTable("games");
+   },
+};
