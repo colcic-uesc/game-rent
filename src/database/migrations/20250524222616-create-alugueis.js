3d8936fe032549e735e9b6d70d5b1148093f7101
Christian Oliveira <christian97_6@live.com>
2025-05-25 18:38:54 -0300

    refactor: standardize migration files by updating field names and formatting

--- src/database/migrations/20250524222616-create-alugueis.js
+++ src/database/migrations/20250524222616-create-alugueis.js
@@ -1,48 +1,47 @@
-'use strict';
+"use strict";
+
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('alugueis', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      id_usuario: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'users', 
-          key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
-      },
-      data_aluguel: {
-        allowNull: false,
-        type: Sequelize.DATE 
-      },
-      data_devolucao: {
-        allowNull: true,
-        type: Sequelize.DATE
-      },
-      status: {
-        allowNull: false,
-        type: Sequelize.ENUM('em andamento', 'finalizado')
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('alugueis');
-  }
+   async up(queryInterface, Sequelize) {
+      await queryInterface.createTable("alugueis", {
+         id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER,
+         },
+         id_usuario: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            references: {
+               model: "users",
+               key: "id",
+            },
+            onUpdate: "CASCADE",
+            onDelete: "CASCADE",
+         },
+         data_aluguel: {
+            allowNull: false,
+            type: Sequelize.DATE,
+         },
+         data_devolucao: {
+            allowNull: true,
+            type: Sequelize.DATE,
+         },
+         status: {
+            allowNull: false,
+            type: Sequelize.ENUM("em andamento", "finalizado"),
+         },
+         created_at: {
+            allowNull: false,
+            type: Sequelize.DATE,
+         },
+         updated_at: {
+            allowNull: false,
+            type: Sequelize.DATE,
+         },
+      });
+   },
+   async down(queryInterface, Sequelize) {
+      await queryInterface.dropTable("alugueis");
+   },
 };
