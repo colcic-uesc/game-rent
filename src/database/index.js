b13a9fc04c412bb7cc64fd38db87542c42d894e0
paollagiselle <paolla12389@gmail.com>
2025-05-25 02:01:13 -0300

    alterações /Alugueis

--- src/database/index.js
+++ src/database/index.js
@@ -6,8 +6,11 @@ import User from "../app/models/User";
 import Game from "../app/models/Game";
 import Genre from "../app/models/Genre";
 import Platform from "../app/models/Platform";
+import Aluguel from "../app/models/Aluguel";
+import AluguelJogos from "../app/models/AluguelJogos";
 
-const models = [User, Platform, Genre, Game];
+
+const models = [User, Platform, Genre, Game, Aluguel,AluguelJogos];
 
 class Database {
    constructor() {
