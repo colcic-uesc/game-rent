8566eb8297f9a6af4b4eae4995727b8fbab2a744
Christian Oliveira <christian97_6@live.com>
2025-05-25 18:39:24 -0300

    refactor: update model imports and standardize field definitions in Aluguel and AluguelJogos

--- src/app/models/Aluguel.js
+++ src/app/models/Aluguel.js
@@ -1,31 +1,34 @@
-const { Model, DataTypes } = require("sequelize");
+"use strict";
+
+import Sequelize, { Model } from "sequelize";
 
 class Aluguel extends Model {
-  static init(sequelize) {
-    super.init(
-      {
-        id_usuario: DataTypes.INTEGER,
-        data_aluguel: DataTypes.DATE,
-        data_devolucao: DataTypes.DATE,
-        status: DataTypes.ENUM('em andamento', 'finalizado'),
-      },
-      {
-        sequelize,
-        tableName: 'alugueis'
-      }
-    );
-  }
+   static init(sequelize) {
+      super.init(
+         {
+            id_usuario: Sequelize.INTEGER,
+            data_aluguel: Sequelize.DATE,
+            data_devolucao: Sequelize.DATE,
+            status: Sequelize.STRING,
+         },
+         {
+            sequelize,
+            tableName: "alugueis",
+         }
+      );
+   }
 
-  static associate(models) {
-    this.belongsTo(models.User, { foreignKey: "id_usuario", as: "usuario" });
+   static associate(models) {
+      this.belongsTo(models.User, { foreignKey: "id_usuario", as: "usuario" });
 
-    // Muitos jogos em um aluguel
-    this.belongsToMany(models.Game, {
-      through: models.AluguelJogos,
-      foreignKey: "id_aluguel",
-      as: "jogos"
-    });
-  }
+      // Muitos jogos em um aluguel
+      this.belongsToMany(models.Game, {
+         through: models.AluguelJogos,
+         as: "jogos",
+         foreignKey: "id_aluguel",
+         otherKey: "id_jogo",
+      });
+   }
 }
 
 module.exports = Aluguel;
--- src/app/models/AluguelJogos.js
+++ src/app/models/AluguelJogos.js
@@ -1,36 +1,32 @@
-const { Model, DataTypes } = require("sequelize");
+"use strict";
+
+import Sequelize, { Model } from "sequelize";
 
 class AluguelJogos extends Model {
-  static init(sequelize) {
-    super.init(
-      {
-        id_aluguel: {
-          type: DataTypes.INTEGER,
-          primaryKey: true
-        },
-        id_jogo: {
-          type: DataTypes.INTEGER,
-          primaryKey: true
-        }
-      },
-      {
-        sequelize,
-        tableName: "aluguel_jogos"
-      }
-    );
-  }
+   static init(sequelize) {
+      super.init(
+         {
+            id_aluguel: Sequelize.INTEGER,
+            id_jogo: Sequelize.INTEGER,
+         },
+         {
+            sequelize,
+            tableName: "aluguel_jogos",
+         }
+      );
+   }
 
-  static associate(models) {
-    this.belongsTo(models.Aluguel, {
-      foreignKey: "id_aluguel",
-      as: "aluguel"
-    });
+   static associate(models) {
+      this.belongsTo(models.Aluguel, {
+         foreignKey: "id_aluguel",
+         as: "aluguel",
+      });
 
-    this.belongsTo(models.Game, {
-      foreignKey: "id_jogo",
-      as: "jogo"
-    });
-  }
+      this.belongsTo(models.Game, {
+         foreignKey: "id_jogo",
+         as: "jogo",
+      });
+   }
 }
 
 module.exports = AluguelJogos;
