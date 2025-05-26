e25e38921402f85fe47f164d6d59299316b75960
Christian Oliveira <christian97_6@live.com>
2025-05-26 00:50:03 -0300

    feat: add user type and active status fields to user seed data

--- src/database/seeds/20250409233852-user.js
+++ src/database/seeds/20250409233852-user.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { is } = require("sequelize/types/lib/operators");
+
 module.exports = {
    up: (queryInterface, Sequelize) => {
       return queryInterface.bulkInsert(
@@ -8,6 +10,8 @@ module.exports = {
             {
                name: "John Doe",
                email: "john@gmail.com",
+               tipo: "admin",
+               is_active: true,
                password_hash: "$2a$08$e0b1c4f8d3a5e7c5f8d3eO9Q6Z5J6F5F5F5F5F5F5F5F5F5F5F5",
                created_at: new Date(),
                updated_at: new Date(),
@@ -15,6 +19,8 @@ module.exports = {
             {
                name: "Jane Doe",
                email: "jane@gmail.com",
+               tipo: "user",
+               is_active: true,
                password_hash: "$2a$08$e0b1c4f8d3a5e7c5f8d3eO9Q6Z5J6F5F5F5F5F5F5F5F5F5F5F5",
                created_at: new Date(),
                updated_at: new Date(),
