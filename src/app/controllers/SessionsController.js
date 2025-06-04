942fe9df80453b1e6b8e22cbf80bcdfe41ea1873
Christian Oliveira <christian97_6@live.com>
2025-06-04 18:35:50 -0300

    fix: update response status code for user not found to 404

--- src/app/controllers/SessionsController.js
+++ src/app/controllers/SessionsController.js
@@ -1,35 +1,35 @@
-import jwt from 'jsonwebtoken';
-import User from '../models/User';
+import jwt from "jsonwebtoken";
+import User from "../models/User";
 
 class SessionsController {
-  async create(req, res) {
-    const { email, password } = req.body;
+   async create(req, res) {
+      const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+      const user = await User.findOne({ where: { email } });
 
-    if (!user) {
-      return res.status(401).json({ error: 'User not found' });
-    }
+      if (!user) {
+         return res.status(404).json({ error: "User not found" });
+      }
 
-    if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Password does not match' });
-    }
+      if (!(await user.checkPassword(password))) {
+         return res.status(401).json({ error: "Password does not match" });
+      }
 
-    const { id, name } = user;
+      const { id, name } = user;
 
-    const token = jwt.sign({ id }, process.env.APP_SECRET, {
-      expiresIn: '7d',
-    });
+      const token = jwt.sign({ id }, process.env.APP_SECRET, {
+         expiresIn: "7d",
+      });
 
-    return res.json({
-      user: {
-        id,
-        name,
-        email,
-      },
-      token,
-    });
-  }
+      return res.json({
+         user: {
+            id,
+            name,
+            email,
+         },
+         token,
+      });
+   }
 }
 
 export default new SessionsController();
