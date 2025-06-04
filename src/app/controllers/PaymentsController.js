fe58430772e0dd7abbc0c485edc8a0573492ec26
IgorCamps <igoruser07@gmail.com>
2025-06-04 13:37:40 -0300

    feat: implementar envio de e-mail de confirmação de pagamento e adicionar métodos de atualização e exclusão

--- src/app/controllers/PaymentsController.js
+++ src/app/controllers/PaymentsController.js
@@ -1,12 +1,17 @@
 import Payment from '../models/Payments';
+import { emailPagamento } from '../../config/apiEmail';
+
 class PaymentsController {
   // Lista todos os pagamentos
   async index(req, res) {
     try {
       const payments = await Payment.findAll();
+      
       return res
          .status(200)
          .json({ message: "Pagamentos listados com sucesso", data: payments });
+        
+            
     } catch (error) {
       return res.status(400).json({
         error: "Erro ao listar pagamentos.",
@@ -35,12 +40,25 @@ class PaymentsController {
   // Cria um novo pagamento
   async create(req, res) {
     try {
-      const { id_aluguel, valor, data_pagamento, metodo, status } = req.body;
-      console.log("Dados do pagamento:", { id_aluguel, valor, data_pagamento, metodo, status });
-      const payment = await Payment.create({ id_aluguel, valor, data_pagamento, metodo, status });
+      const { id_aluguel, valor, metodo, status } = req.body;
+      // console.log("Dados do pagamento:", { id_aluguel, valor, metodo, status });
+      const payment = await Payment.create({ id_aluguel, valor, metodo, status });
+      
+
+      if (status == 'aprovado') {
+        
+      emailPagamento(
+        "Igor",
+        "igoruser07@gmail.com",
+        req.body.valor,
+      );
+      }
+
+
       return res
         .status(201)
-        .json({ message: "Pagamento criado com sucesso", data: payment });
+        .json({ message: "Pagamento criado com sucesso", data: payment })
+        .send(emailContent);
     } catch (error) {
       return res.status(400).json({
         error: "Erro ao criar pagamento.",
@@ -49,5 +67,46 @@ class PaymentsController {
     }
   }
 
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const { id_aluguel, valor, metodo, status } = req.body;
+
+      const payment = await Payment.findByPk(id);
+      if (!payment) {
+        return res.status(404).json({ error: "Pagamento não encontrado." });
+      }
+
+      await payment.update({ id_aluguel, valor, metodo, status });
+
+      return res
+        .status(200)
+        .json({ message: "Pagamento atualizado com sucesso", data: payment });
+    } catch (error) {
+      return res.status(400).json({
+        error: "Erro ao atualizar pagamento.",
+        details: error.message,
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      const payment = await Payment.findByPk(id);
+      if (!payment) {
+        return res.status(404).json({ error: "Pagamento não encontrado." });
+      }
+
+      await payment.destroy();
+
+      return res.status(200).json({ message: "Pagamento deletado com sucesso" });
+    } catch (error) {
+      return res.status(400).json({
+        error: "Erro ao deletar pagamento.",
+        details: error.message,
+      });
+    }
+  }
 }
 export default new PaymentsController();
--- src/config/apiEmail.js
+++ src/config/apiEmail.js
@@ -4,21 +4,34 @@ const transporter = nodemailer.createTransport({
   host: 'sandbox.smtp.mailtrap.io',
   port: 2525,
   auth: {
-    user: 'c5b38a8bc7ad52',
-    pass: 'bab7d286d693f6'
+    user: '29ae0b476a1744',
+    pass: '942a8ffd5ab593'
   }
 });
 
-transporter.sendMail({
-  from: '"Seu Projeto" <test@meuprojeto.com>',
-  to: 'fernandagatinha2@gmail.com',
-  subject: 'Testando Mailtrap',
-  text: 'Olá, Fernanda Gatinha. Assine nossa internt!',
-  html: '<p><strong>Tudo bem? Podemos conversar?</strong> com <em>Mailtrap</em>.</p>'
-})
+export function emailPagamento(nome, email, valor) {
+  const message = {
+  from: 'nao responda <equipegamerent@games.dev',
+  to: `${email}`,
+  subject: 'Confirmação de Pagamento',
+  text: `Olá ${nome}`,
+  html: '<p><strong>Tudo bem? Obrigado por assinar nosso serviço!</strong>' +
+        '<p>A equipe da Game Rent agradece pela sua compra! Muito obrigado por escolher nossos serviços.</p>' +
+        '<p>Estamos sempre disponiveis com novidades incriveis juntamente com promoções imperdiveis</p>' +
+        '<p>acesse nossos canais de atendimentos e redes sociais e fique por dentro de tudo!</p>' +
+        '<p>Stay gamer!</p>' +
+        `<p>Valor do pagamento: R$ ${valor}</p>` +
+        '<p>Se você tiver alguma dúvida ou precisar de assistência, não hesite em entrar em contato conosco.</p>' +
+        '<p>Atenciosamente,</p>' +
+        '<p>Equipe Game Rent</p>'
+}
+
+transporter.sendMail(message)
 .then(() => {
   console.log("E-mail enviado com sucesso para a caixa Mailtrap.");
 })
 .catch((err) => {
   console.error("Erro ao enviar e-mail:", err);
 });
+
+}
