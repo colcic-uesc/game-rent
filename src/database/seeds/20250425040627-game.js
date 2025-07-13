5d1d73a1f83505589135d2c133d1ccbe0006f6be
Christian Oliveira <christian97_6@live.com>
2025-07-13 04:56:01 -0300

    Update 20250425040627-game.js

--- src/database/seeds/20250425040627-game.js
+++ src/database/seeds/20250425040627-game.js
@@ -1,52 +1,54 @@
 "use strict";
 
 module.exports = {
-   up: (queryInterface) => {
-      return queryInterface.bulkInsert(
-         "games",
-         [
-            {
-               title: "Death Stranding 2",
-               description: "Ação e aventura em um mundo estranho. Bem a cara do Kojima!",
-               price: 49.90,
-               status: "available",
-               yt_link: "eT_A2gPhTIw",
-               capa_jogo: "death_stranding_2_ps4.jpg",
-               platform_id: 5,
-               genre_id: 5,
-               created_at: new Date(),
-               updated_at: new Date(),
-            },
-            {
-               title: "Elden Ring",
-               description: "Muitas confusões em um mundo em que uma porrada só pode te matar.",
-               price: 49.90,
-               status: "available",
-               yt_link: "K_03kFqWfqs",
-               capa_jogo: "elden_ring_ps5.jpg",
-               platform_id: 5,
-               genre_id: 7,
-               created_at: new Date(),
-               updated_at: new Date(),
-            },
-            {
-               title: "Sniper Elite 5",
-               description: "Uma headshot à 5km bem dado e eu dormia mansinho.",
-               price: 49.90,
-               status: "available",
-               yt_link: "K_03kFqWfqs",
-               capa_jogo: "elden_ring_ps5.jpg",
-               platform_id: 4,
-               genre_id: 7,
-               created_at: new Date(),
-               updated_at: new Date(),
-            },
-         ],
-         {}
-      );
-   },
+  up: (queryInterface) => {
+    return queryInterface.bulkInsert(
+      "games",
+      [
+        {
+          title: "Death Stranding 2",
+          description:
+            "Ação e aventura em um mundo em se não cremar o presunto ele vira uma bomba atômica. Bem a cara do Kojima!",
+          price: 49.9,
+          status: "available",
+          yt_link: "eT_A2gPhTIw",
+          capa_jogo: "death_stranding_2_ps4.jpg",
+          platform_id: 5,
+          genre_id: 5,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          title: "Elden Ring",
+          description:
+            "Muitas confusões em um mundo em que uma porrada só pode te matar.",
+          price: 49.9,
+          status: "available",
+          yt_link: "K_03kFqWfqs",
+          capa_jogo: "elden_ring_ps5.jpg",
+          platform_id: 5,
+          genre_id: 7,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          title: "Sniper Elite 5",
+          description: "Uma headshot à 5km bem dado e eu dormia mansinho.",
+          price: 49.9,
+          status: "available",
+          yt_link: "K_03kFqWfqs",
+          capa_jogo: "elden_ring_ps5.jpg",
+          platform_id: 4,
+          genre_id: 7,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
 
-   down: (queryInterface) => {
-      return queryInterface.bulkDelete("games", null, {});
-   },
+  down: (queryInterface) => {
+    return queryInterface.bulkDelete("games", null, {});
+  },
 };
