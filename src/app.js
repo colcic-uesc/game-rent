e5f61318761217946d74c70772fbafe98fabae78
Christian Oliveira <christian97_6@live.com>
2025-07-13 04:54:35 -0300

    feat: Serve static files from public directory
    This adds a middleware to serve static files , such as images, from the
    public directory.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import express from "express";
 import Youch from "youch";
 import "express-async-errors";
 import cors from "cors";
+import path from "path";
 
 import { specs, swaggerUi } from "./config/swagger";
 
@@ -12,35 +13,39 @@ import routes from "./routes";
 import "./database/index";
 
 class App {
-   constructor() {
-      this.server = express();
+  constructor() {
+    this.server = express();
 
-      this.middlewares();
-      this.routes();
-      this.exceptionHandler();
-   }
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
+  }
 
-   middlewares() {
-      this.server.use(cors());
-      this.server.use(express.json());
-      this.server.use(express.urlencoded({ extended: false }));
-      this.server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
-   }
+  middlewares() {
+    this.server.use(cors());
+    this.server.use(express.json());
+    this.server.use(express.urlencoded({ extended: false }));
+    this.server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+    this.server.use(
+      "/files",
+      express.static(path.join(__dirname, "..", "public"))
+    );
+  }
 
-   routes() {
-      this.server.use(routes);
-   }
+  routes() {
+    this.server.use(routes);
+  }
 
-   exceptionHandler() {
-      this.server.use(async (err, req, res, next) => {
-         if (process.env.NODE_ENV === "development") {
-            const errors = await new Youch(err, req).toJSON();
-            return res.status(500).json(errors);
-         }
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === "development") {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
 
-         return res.status(500).json({ error: "Internal server error" });
-      });
-   }
+      return res.status(500).json({ error: "Internal server error" });
+    });
+  }
 }
 
 export default new App().server;
