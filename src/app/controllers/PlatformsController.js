3265d6dc3a528d3c39eff995d703ff4880d16652
Christian Oliveira <christian97_6@live.com>
2025-07-13 04:58:49 -0300

    feat: soft deletes a platform, by updating the is_active field and also soft delete all games associated with the platform.

--- src/app/controllers/PlatformsController.js
+++ src/app/controllers/PlatformsController.js
@@ -1,98 +1,108 @@
 import * as Yup from "yup";
 
 import Platform from "../models/Platform";
+import Game from "../models/Game";
 
 class PlatformsController {
-   async index(req, res) {
-      const platforms = await Platform.findAll({
-         order: [["name", "ASC"]],
-         attributes: ["id", "name", "createdAt", "updatedAt"],
+  async index(req, res) {
+    const platforms = await Platform.findAll({
+      order: [["name", "ASC"]],
+      attributes: ["id", "name", "is_active", "createdAt", "updatedAt"],
+    });
+
+    return res.json(platforms);
+  }
+
+  async show(req, res) {
+    const platform = await Platform.findByPk(req.params.id);
+
+    if (!platform) {
+      return res.status(404).json({
+        message: "Plataforma não encontrada.",
       });
+    }
 
-      return res.json(platforms);
-   }
+    return res.json(platform);
+  }
 
-   async show(req, res) {
-      const platform = await Platform.findByPk(req.params.id);
+  async create(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required("Nome da plataforma é obrigatório"),
+    });
 
-      if (!platform) {
-         return res.status(404).json({
-            message: "Plataforma não encontrada.",
-         });
-      }
-
-      return res.json(platform);
-   }
-
-   async create(req, res) {
-      const schema = Yup.object().shape({
-         name: Yup.string().required("Nome da plataforma é obrigatório"),
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: "Erro na validação dos dados.",
       });
+    }
 
-      if (!(await schema.isValid(req.body))) {
-         return res.status(400).json({
-            error: "Erro na validação dos dados.",
-         });
-      }
+    const platform = await Platform.findOne({
+      where: {
+        name: req.body.name,
+      },
+    });
 
-      const platform = await Platform.findOne({
-         where: {
-            name: req.body.name,
-         },
+    if (platform) {
+      return res.status(400).json({
+        error: "Plataforma já existe.",
       });
+    }
 
-      if (platform) {
-         return res.status(400).json({
-            error: "Plataforma já existe.",
-         });
-      }
+    await Platform.create(req.body);
 
-      await Platform.create(req.body);
+    return res.status(201).json({
+      message: `Plataforma criada com sucesso`,
+    });
+  }
 
-      return res.status(201).json({
-         message: `Plataforma criada com sucesso`,
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required("Nome da plataforma é obrigatório"),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: "Erro na validação dos dados.",
       });
-   }
+    }
 
-   async update(req, res) {
-      const schema = Yup.object().shape({
-         name: Yup.string().required("Nome da plataforma é obrigatório"),
+    const platform = await Platform.findByPk(req.params.id);
+
+    if (!platform) {
+      return res.status(404).json({
+        message: "Plataforma não encontrada.",
       });
+    }
 
-      if (!(await schema.isValid(req.body))) {
-         return res.status(400).json({
-            error: "Erro na validação dos dados.",
-         });
-      }
+    await platform.update(req.body);
 
-      const platform = await Platform.findByPk(req.params.id);
+    return res
+      .status(200)
+      .json({ message: "Plataforma atualizada com sucesso" });
+  }
 
-      if (!platform) {
-         return res.status(404).json({
-            message: "Plataforma não encontrada.",
-         });
-      }
+  async destroy(req, res) {
+    const platform = await Platform.findByPk(req.params.id);
 
-      await platform.update(req.body);
+    if (!platform) {
+      return res.status(404).json({ message: "Plataforma não encontrada." });
+    }
 
-      return res
-         .status(200)
-         .json({ message: "Plataforma atualizada com sucesso" });
-   }
+    platform.is_active = !platform.is_active;
 
-   async destroy(req, res) {
-      const platform = await Platform.findByPk(req.params.id);
+    await platform.save();
 
-      if (!platform) {
-         return res.status(404).json({ message: "Plataforma não encontrada." });
-      }
+    await Game.update(
+      { is_active: platform.is_active },
+      { where: { platform_id: platform.id } }
+    );
 
-      await platform.destroy();
+    const message = platform.is_active
+      ? "Plataforma reativada com sucesso"
+      : "Plataforma desativada com sucesso";
 
-      return res
-         .status(204)
-         .json({ message: "Plataforma excluída com sucesso" });
-   }
+    return res.status(200).json({ message });
+  }
 }
 
 export default new PlatformsController();
